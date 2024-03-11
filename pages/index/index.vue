52981148d9aa89dd7b1c294ff8c2d87ca1c842b0
全易 <quanyi@edo-iot.com>
2024-03-11 15:23:11 +0800

    updata

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,7 +2,6 @@
 	<view class="m-5">
 		<button plain type="primary" @tap="toPage('/pages/demo/unocss')">unocss demo</button>
 		<button plain type="primary" @tap="toPage('/pages/demo/vue3')" class="mt-3">vue3 demo</button>
-		<button plain type="primary" @tap="toPage('/pages/demo/tuniao')" class="mt-3">tuniao demo</button>
 		<button plain type="primary" @tap="toPage('/pages/demo/pinia')" class="mt-3">pinia demo</button>
 	</view>
 </template>
