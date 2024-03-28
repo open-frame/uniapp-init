e1c556e4a000d2319dfb9cab15cb6761398db905
全易 <771184240@qq.com>
2024-03-28 21:23:18 +0800

    vue2 ban

--- store/modules/custom.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useCustomStore = defineStore("custom", {
-	state: () => ({
-		customInfo: uni.getMenuButtonBoundingClientRect()
-	}),
-})
--- store/modules/system.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useSystemStore = defineStore("system", {
-	state: () => ({
-		systemInfo: uni.getSystemInfoSync(),
-	}),
-})
